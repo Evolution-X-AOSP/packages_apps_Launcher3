7991df3cb7de6caaf3d886be9a25684fdc829906
Joker-V2 <nipinna0@gmail.com>
2022-12-20 12:37:39 +0000

    Launcher3: Set default recents background opacity to 100
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -914,7 +914,7 @@ public final class Utilities {
 
     public static int getRecentsOpacity(Context context) {
         SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
-        return prefs.getInt(KEY_RECENTS_OPACITY, 40);
+        return prefs.getInt(KEY_RECENTS_OPACITY, 100);
     }
 
     public static int getAllAppsOpacity(Context context) {
