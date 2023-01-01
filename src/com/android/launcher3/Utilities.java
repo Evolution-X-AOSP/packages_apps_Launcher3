9cd9268ef5ed487b7df952828729604796d23e5b
AnierinB <anierin@evolution-x.org>
2023-01-01 05:37:46 +0000

    Launcher3: Set default app drawer opacity to 100%
    To better match stock Pixel launcher
    
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -1087,7 +1087,7 @@ public final class Utilities {
 
     public static int getAllAppsOpacity(Context context) {
         SharedPreferences prefs = getPrefs(context.getApplicationContext());
-        return prefs.getInt(KEY_APP_DRAWER_OPACITY, 80);
+        return prefs.getInt(KEY_APP_DRAWER_OPACITY, 100);
     }
 
     public static boolean showSmartspace(Context context) {
