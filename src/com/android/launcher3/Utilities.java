c9dea5da4c338d4537e834cd4e1c39a289e45568
Pranav Vashi <neobuddy89@gmail.com>
2023-02-09 01:44:31 +0530

    Launcher3: Add customization to set hotseat background opacity
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Change-Id: Ib75060af027547abdd09d6f5b9259a7e2d6854ab

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -173,6 +173,7 @@ public final class Utilities {
     public static final String KEY_RECENTS_MEMINFO = "pref_recents_meminfo";
     public static final String KEY_DRAWER_SEARCH = "pref_drawer_search";
     public static final String KEY_SMARTSPACE = "pref_smartspace";
+    public static final String KEY_HOTSEAT_OPACITY = "pref_hotseat_opacity";
 
     @IntDef({TRANSLATE_UP, TRANSLATE_DOWN, TRANSLATE_LEFT, TRANSLATE_RIGHT})
     public @interface AdjustmentDirection{}
@@ -927,5 +928,10 @@ public final class Utilities {
     private static boolean isSmartspaceEnabled(Context context) {
         SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
         return prefs.getBoolean(KEY_SMARTSPACE, true);
+    }
+
+    public static int getHotseatOpacity(Context context) {
+        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
+        return prefs.getInt(KEY_HOTSEAT_OPACITY, 40);
     }
 }
